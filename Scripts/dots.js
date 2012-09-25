169d08c0c0cf659727b1433ec60e6c50e49bae3b
Ryan Fiorini <ryanwfiorini@gmail.com>
2012-09-24 20:44:43 -0400

    Major cleanup of code

--- Scripts/dots.js
+++ Scripts/dots.js
@@ -1,168 +1,183 @@
-﻿var MAX_ROWS = 5;
-var MAX_COLS = 5;
-var x = 310, y = 180, r = 10
+﻿/*! rf.dots() Dots game builder using Raphael in JS. Authors & copyright (c) 2012: Ryan Fiorini. Dual MIT/BSD license */
+window.rf = window.rf || {};
 
-var playerUp = 1;
-var player1Blocks = 0;
-var player2Blocks = 0;
+if (!window.rf.dots || typeof (dots) !== "function") {
+    window.rf.dots = (function () {
+        "use strict";
+        var options1 = {
+            MAX_ROWS: 5,
+            MAX_COLS: 5,
+            x: 310,
+            y: 180,
+            r: 10,
 
-var circles = null;
-var lines = null;
-var squares = null;
+            playerUp: 1,
+            player1Blocks: 0,
+            player2Blocks: 0
+        };
 
-function Circle(row, col, selected) {
-    return {
-        row: row,
-        col: col,
-        selected: selected
-    }
-}
+        return {
+            options: {
+                MAX_ROWS: 5,
+                MAX_COLS: 5,
+                x: 310,
+                y: 180,
+                r: 10,
 
-// Square Class
-function Square(row, col) {
-    return {
-        row: row,
-        col: col,
-        top: false,
-        right: false,
-        bottom: false,
-        left: false,
-        captured: false
-    }
-}
+                playerUp: 1,
+                player1Blocks: 0,
+                player2Blocks: 0
+            },
+            board: {
+                circles: null,
+                lines: null,
+                squares: null
+            },
 
-function loadCircles() {
-    circles = new Array(MAX_ROWS);
+            Circle: function (row, col, selected) {
+                return {
+                    row: row,
+                    col: col,
+                    selected: selected
+                }
+            },
 
+            Square: function (row, col) {
+                return {
+                    row: row,
+                    col: col,
+                    top: false,
+                    right: false,
+                    bottom: false,
+                    left: false,
+                    captured: false
+                }
+            },
 
-    // create the array of circles
-    for (var i = 0; i < MAX_ROWS; i++) {
-        circles[i] = new Array(MAX_COLS);
-        for (var j = 0; j < MAX_COLS; j++) {
-            circles[i][j] = new Circle(i, j, false);
+            loadCircles: function () {
+                this.board.circles = new Array(this.options.MAX_ROWS);
 
-            var anim = Raphael.animation({cx: 50 * (i + 1), cy: 50 * (j + 1)}, 2e3);
-            //R.ball(10, 10, 10, Math.random()).animate(anim);
-            R.ball(10, 10, 10).attr({id:"circle_" + i + "_" + j}).animate(anim);
-        }
-    }
-}
+                // create the array of circles
+                for (var i = 0; i < this.options.MAX_ROWS; i++) {
+                    this.board.circles[i] = new Array(this.options.MAX_COLS);
+                    for (var j = 0; j < this.options.MAX_COLS; j++) {
+                        this.board.circles[i][j] = new this.Circle(i, j, false);
 
-function loadSquares() {
-    squares = new Array(MAX_ROWS - 1);
+                        var anim = Raphael.animation({ cx: 50 * (i + 1), cy: 50 * (j + 1) }, 2e3);
+                        //R.ball(10, 10, 10, Math.random()).animate(anim);
+                        R.ball(10, 10, 10).attr({ id: "circle_" + i + "_" + j }).animate(anim);
+                    }
+                }
+            },
 
-    // add the center squares.
-    for (var i = 0; i < (MAX_ROWS-1); i++) {
-        squares[i] = new Array(MAX_COLS - 1);
-        for (var j = 0; j < (MAX_COLS-1); j++) {
-            squares[i][j] = new Square(i, j);
-            var anim = Raphael.animation({x: (50 * (i + 1))+3, y: 50 * (j + 1)+3}, 2e3);
+            loadSquares: function () {
+                this.board.squares = new Array(this.options.MAX_ROWS - 1);
 
-            var squ = R.rect(10, 10, 44, 44).attr({gradient: '90-#526c7a-#64a0c1'}).click(function () {
-                this.attr({fill: "#000"})
-            }).animate(anim);  
+                // add the center squares.
+                for (var i = 0; i < (this.options.MAX_ROWS - 1); i++) {
+                    this.board.squares[i] = new Array(this.options.MAX_COLS - 1);
+                    for (var j = 0; j < (this.options.MAX_COLS - 1); j++) {
+                        this.board.squares[i][j] = new this.Square(i, j);
+                        var anim = Raphael.animation({ x: (50 * (i + 1)) + 3, y: 50 * (j + 1) + 3 }, 2e3);
 
-            squares[i][j].box = squ;
-        }
-    }
+                        var squ = R.rect(10, 10, 44, 44).attr({ gradient: '90-#526c7a-#64a0c1' }).click(function () {
+                            this.attr({ fill: "#000" })
+                        }).animate(anim);
 
-}
+                        this.board.squares[i][j].box = squ;
+                    }
+                }
+            },
 
-function loadClickLines() {
-    // add the Vertical lines to the document.
-    for (var i = 0; i < (MAX_ROWS); i++) {
-        for (var j = 0; j < (MAX_COLS-1); j++) {
-            var anim = Raphael.animation({x: (50 * (i + 1))-3, y: 50 * (j + 1)}, 2e3);
+            loadClickLines: function () {
+                var that = this;
 
-            var t = R.rect(10, 10, 6, 50).attr({fill: "#ccc"}).click(function () {
-                this.attr({fill: "#f00",gradient: '90-#526c7a-#64a0c1'})
-            }).animate(anim);  
-            t.node.id = "hline_" + i + "_" + j;
-        }
-    }
+                // add the Vertical lines to the document.
+                for (var i = 0; i < (this.options.MAX_ROWS); i++) {
+                    for (var j = 0; j < (this.options.MAX_COLS - 1); j++) {
+                        var anim = Raphael.animation({ x: (50 * (i + 1)) - 3, y: 50 * (j + 1) }, 2e3);
 
-    // add the Horizontal lines to the document.
-    for (var i = 0; i < (MAX_ROWS-1); i++) {
-        for (var j = 0; j < (MAX_COLS); j++) {
-            var anim = Raphael.animation({x: 50 * (i + 1), y: 50 * (j + 1)-3}, 2e3);
+                        var t = R.rect(10, 10, 6, 50).attr({ fill: "#ccc" }).click(function () {
+                            this.attr({ fill: "#f00", gradient: '90-#526c7a-#64a0c1' })
+                        }).animate(anim);
+                        t.node.id = "hline_" + i + "_" + j;
+                    }
+                }
 
-            var t = R.rect(10, 10, 50, 6).attr({fill: "#ccc"}).click(function () {
-                //this.attr({gradient: '90-#526c7a-#64a0c1'})
-                handleHorizontalLineClick(this);
-            }).animate(anim);
-            t.node.id = "hline_" + i + "_" + j;
-        }
-    }
-}
+                // add the Horizontal lines to the document.
+                for (var i = 0; i < (this.options.MAX_ROWS - 1); i++) {
+                    for (var j = 0; j < (this.options.MAX_COLS); j++) {
+                        var anim = Raphael.animation({ x: 50 * (i + 1), y: 50 * (j + 1) - 3 }, 2e3);
 
-function handleHorizontalLineClick(line) {
-    debugger;
-    //$("#" + this.id).unbind("click");
-    line.attr({ gradient: '90-#526c7a-#64a0c1' });
-    var captured = false;
-    var rowcol = getRowCol(line);
+                        var t = R.rect(10, 10, 50, 6).attr({ fill: "#ccc" }).click(function () {
+                            that.handleHorizontalLineClick(this);
+                        }).animate(anim);
+                        t.node.id = "hline_" + i + "_" + j;
+                    }
+                }
+            },
 
-    if (rowcol.row == 0) {
-        squares[rowcol.row][rowcol.col].top = true;
+            handleHorizontalLineClick: function (line) {
+                line.attr({ gradient: '90-#526c7a-#64a0c1' });
+                var captured = false;
+                var rowcol = this.getRowCol(line);
 
-        if (squares[rowcol.row][rowcol.col].top &&
-                squares[rowcol.row][rowcol.col].right &&
-                squares[rowcol.row][rowcol.col].bottom &&
-                squares[rowcol.row][rowcol.col].left) {
-            /*$("#" + squares[rowcol.row][rowcol.col].box.id).addClass((playerUp == 1) ? "box_player1" : "box_player2");
-            if (playerUp == 1) {
-            player1Blocks += 1;
-            $("#player1Blocks").html("Player 1 Blocks: " + player1Blocks);
-            } else {
-            player2Blocks += 1;
-            $("#player2Blocks").html("Player 2 Blocks: " + player2Blocks);
-            }*/
-            setCapturedSquare(rowcol.row, rowcol.col);
-            captured = true;
-        }
-    } else if (rowcol.row == (MAX_ROWS - 1)) {
-        squares[rowcol.row - 1][rowcol.col].bottom = true;
+                if (rowcol.row == 0) {
+                    this.board.squares[rowcol.row][rowcol.col].top = true;
 
-        if (squares[rowcol.row - 1][rowcol.col].top &&
-                squares[rowcol.row - 1][rowcol.col].right &&
-                squares[rowcol.row - 1][rowcol.col].bottom &&
-                squares[rowcol.row - 1][rowcol.col].left) {
-            setCapturedSquare(rowcol.row - 1, rowcol.col);
-            captured = true;
-        }
-    } else {
-        squares[rowcol.row][rowcol.col].top = true;
-        squares[rowcol.row - 1][rowcol.col].bottom = true;
+                    if (this.board.squares[rowcol.row][rowcol.col].top &&
+                            this.board.squares[rowcol.row][rowcol.col].right &&
+                            this.board.squares[rowcol.row][rowcol.col].bottom &&
+                            this.board.squares[rowcol.row][rowcol.col].left) {
+                        setCapturedSquare(rowcol.row, rowcol.col);
+                        captured = true;
+                    }
+                } else if (rowcol.row == (this.options.MAX_ROWS - 1)) {
+                    this.board.squares[rowcol.row - 1][rowcol.col].bottom = true;
 
-        if (squares[rowcol.row][rowcol.col].top &&
-                squares[rowcol.row][rowcol.col].right &&
-                squares[rowcol.row][rowcol.col].bottom &&
-                squares[rowcol.row][rowcol.col].left) {
-            setCapturedSquare(rowcol.row, rowcol.col);
-            captured = true;
-        }
+                    if (this.board.squares[rowcol.row - 1][rowcol.col].top &&
+                            this.board.squares[rowcol.row - 1][rowcol.col].right &&
+                            this.board.squares[rowcol.row - 1][rowcol.col].bottom &&
+                            this.board.squares[rowcol.row - 1][rowcol.col].left) {
+                        setCapturedSquare(rowcol.row - 1, rowcol.col);
+                        captured = true;
+                    }
+                } else {
+                    this.board.squares[rowcol.row][rowcol.col].top = true;
+                    this.board.squares[rowcol.row - 1][rowcol.col].bottom = true;
 
-        if (squares[rowcol.row - 1][rowcol.col].top &&
-                squares[rowcol.row - 1][rowcol.col].right &&
-                squares[rowcol.row - 1][rowcol.col].bottom &&
-                squares[rowcol.row - 1][rowcol.col].left) {
-            setCapturedSquare(rowcol.row - 1, rowcol.col);
-            captured = true;
-        }
-    }
+                    if (this.board.squares[rowcol.row][rowcol.col].top &&
+                            this.board.squares[rowcol.row][rowcol.col].right &&
+                            this.board.squares[rowcol.row][rowcol.col].bottom &&
+                            this.board.squares[rowcol.row][rowcol.col].left) {
+                        setCapturedSquare(rowcol.row, rowcol.col);
+                        captured = true;
+                    }
 
-    if (!captured) {
-        playerUp = (playerUp == 1) ? 2 : 1;
-        $("#currentPlayer").html("Current Player: " + playerUp);
-    }
-}
+                    if (this.board.squares[rowcol.row - 1][rowcol.col].top &&
+                            this.board.squares[rowcol.row - 1][rowcol.col].right &&
+                            this.board.squares[rowcol.row - 1][rowcol.col].bottom &&
+                            this.board.squares[rowcol.row - 1][rowcol.col].left) {
+                        setCapturedSquare(rowcol.row - 1, rowcol.col);
+                        captured = true;
+                    }
+                }
 
-function getRowCol(htmlDiv) {
-    var id = htmlDiv.node.id;
-    var locFirst = id.indexOf("_");
-    var locSecond = id.lastIndexOf("_");
+                if (!captured) {
+                    this.options.playerUp = (this.options.playerUp == 1) ? 2 : 1;
+                    $("#currentPlayer").html("Current Player: " + this.options.playerUp);
+                }
+            },
 
-    var row = id.substring(locFirst + 1, locSecond);
-    var col = id.substring(locSecond + 1, id.length);
-    return { row: parseInt(row), col: parseInt(col) };
+            getRowCol: function (htmlDiv) {
+                var id = htmlDiv.node.id;
+                var locFirst = id.indexOf("_");
+                var locSecond = id.lastIndexOf("_");
+
+                var row = id.substring(locFirst + 1, locSecond);
+                var col = id.substring(locSecond + 1, id.length);
+                return { row: parseInt(row), col: parseInt(col) };
+            }
+        };
+    })();
 }
